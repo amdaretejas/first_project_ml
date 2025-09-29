184aeac6dfacbe0f618efd25422df0eee905cbab
amdaretejas <amdaretejas1@gmail.com>
2025-09-30 02:36:33 +0530

    data Ingestion

---
+++ run.py
@@ -0,0 +1,7 @@
+# run.py
+from src.components.data_ingestion import DataIngestion
+
+# Call the function you want to test
+# initiate_data_ingestion()
+obj=DataIngestion()
+obj.initiate_data_ingestion()
